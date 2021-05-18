401c236112a8a86a51b6e405261c546b4ac19023
Elton Campos <eltoncampos36@gmail.com>
2021-05-17 23:33:16 -0300

    chore: create accordion

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-if (tabContent.length && tabContent) {
-    tabContent[0].classList.add('ativo')
+    if (tabContent.length && tabContent) {
+        tabContent[0].classList.add('ativo')
 
-    function activeTab(index) {
-        tabContent.forEach(section => {
-            section.classList.remove('ativo')
+        function activeTab(index) {
+            tabContent.forEach(section => {
+                section.classList.remove('ativo')
+            })
+
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((menuItem, index) => {
+            menuItem.addEventListener('click', () => {
+                activeTab(index)
+            })
         })
+    }
+}
 
-        tabContent[index].classList.add('ativo')
+initTabNav();
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    if ( accordionList.length) {
+        accordionList[0].classList.add('ativo')
+        accordionList[0].nextElementSibling.classList.add('ativo')
+
+        function activeAccordion() {
+            this.classList.toggle('ativo')
+            this.nextElementSibling.classList.toggle('ativo')
+        }
+
+
+        accordionList.forEach(item => {
+            item.addEventListener('click', activeAccordion)
+        })
     }
 
-    tabMenu.forEach((menuItem, index) => {
-        menuItem.addEventListener('click', () => {
-            activeTab(index)
-        })
-    })
 }
+
+initAccordion();
